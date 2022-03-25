66d420ecc4bcd27d075de49434519720a8340606
NubTheFatMan <that1nub@gmail.com>
2022-03-25 17:31:08 -0500

    Updated tokens command

--- plugins/commands/tokens.js
+++ plugins/commands/tokens.js
@@ -2,13 +2,30 @@ exports.type = "command";
 
 exports.structure = {
     name: "tokens",
-    description: "Code Sensei will complete the snippet you provide."
+    description: "View your tokens."
 }
 
 exports.onCall = (interaction, data) => {
     let embed = new Discord.MessageEmbed();
     embed.setColor(0x0096ff);
-    embed.setDescription(`${emotes.information} Your last question, that was asked <t:${Math.round(data.lastAskedTimestamp / 1000)}:R>, used **${data.lastTokensUsed}** tokens. You have __${data.tokens}__ tokens remaining.\nWant more? Visit [the token shop](${tokenShop}) to buy more, or wait till <t:${Math.round(resetTime.getTime() / 1000)}> when they're reset!\nOverall, you have spent **${data.spentTokens}** tokens. Accumulatively, you have had **${data.totalTokens}** tokens total.`);
+    embed.setTitle("Tokens");
+    embed.setDescription(`You have **${data.tokens}** tokens.`);
+
+    embed.addField("⏱️ Last Question:", 
+        data.lastAskedTimestamp > 0 ? 
+        `Your last question was asked <t:${Math.round(data.lastAskedTimestamp / 1000)}:R>. It used **${data.lastTokensUsed}** tokens.` : 
+        "You have never asked a question before! Try asking one!"
+    );
+
+    embed.addField("🔻 Spent Tokens:",
+        data.spentTokens > 0 ?
+        `You have spent **${data.spentTokens}** tokens.` :
+        "You have never spent tokens before! Use commands that use OpenAI to spend tokens!"
+    );
+
+    embed.addField("🔺 Accumulated Tokens:", `You have earned **${data.totalTokens}** tokens.`);
+
+    embed.addField("💰 Want more tokens?", `Visit [the token shop](${tokenShop}) to buy more, or wait till <t:${Math.round(resetTime.getTime() / 1000)}> when they're reset!`);
 
     interaction.reply({embeds: [embed]});
 }
